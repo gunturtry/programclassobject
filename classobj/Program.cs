115bfdc47d1e3cd9652634d1d61636023af54ff1
Gunturaa <gunturtriatmaja@students.amikom.ac.id>
2023-03-26 21:28:13 +0700

    tugas prak3

---
+++ classobj/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classobj
+{
+    public class Program
+        {
+            public static void Main()
+            {
+            Mobil mobil1 = new Mobil();
+            
+            mobil1.Warna = "Hitam";
+            mobil1.JumlahPintu = 4;
+            mobil1.Merk = "Jeep";
+            mobil1.Model = "Rubicon";
+            mobil1.TahunKeluar = 2021;
+            
+            mobil1.Gas(80);
+            mobil1.Klakson("poom");
+            mobil1.Tampilkaninfo();
+
+
+
+            Console.Write("Press <Q> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.Q) { }
+        }
+        }
+
+       
+
+        }
+ 
